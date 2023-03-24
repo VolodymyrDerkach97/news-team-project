c93625d4d8c249c4a0d209315ab28766f12ad959
VolodymyrDerkach97 <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-03-24 09:07:41 +0000

    Deploying to gh-pages from @ VolodymyrDerkach97/news-team-project@ac7ec98f4e331ca4c89084a767b68edbd99c04b3 🚀

---
+++ index.6ce9de57.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-box__icon"),c=document.querySelector(".search-box"),o=document.querySelector(".close-icon");e.onclick=function(){c.classList.add("active")},o.addEventListener("click",(function(){return c.classList.remove("active")}))}();
+//# sourceMappingURL=index.6ce9de57.js.map
