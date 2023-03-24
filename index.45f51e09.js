2593f367f8f0080d88cad2a2bd041271fa9d6381
VolodymyrDerkach97 <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-03-24 19:37:46 +0000

    Deploying to gh-pages from @ VolodymyrDerkach97/news-team-project@825356a235278a5ccbd4c100c391030c5987fc58 🚀

--- index.45f51e09.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-box__icon"),c=document.querySelector(".search-box"),o=document.querySelector(".close-icon");e.onclick=()=>{c.classList.add("active")},o.addEventListener("click",(()=>c.classList.remove("active")));
-//# sourceMappingURL=index.45f51e09.js.map
